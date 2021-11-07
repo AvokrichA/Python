99d50bb11ac3a4e2763adf47c845fdb9e9fdccd9
AvokrichA <leysanspb@mail.ru>
2021-11-08 00:39:16 +0300

    Все задания в одном файле, закомментированы

---
+++ HomeTask1.py
@@ -0,0 +1,29 @@
+#ЗАДАНИЕ 1
+#user_name = input ("Представьтесь")
+#print(user_name)
+#phone_user = input ("Введите номер телефона")
+#print(phone_user)
+#print(f"user_name, спасибо, что обратились к нам")
+#str1= f"Здравствуйте, {user_name}! Спасибо за обращние в нашу службу! Наш специалист свяжется с вами по телефону {phone_user} завтра с 9-00 до 10-00"
+#print(str1)
+
+# ЗАДАНИЕ 2
+#sec = int(input("Сколько секунд?"))
+#hour = sec//3600
+#sec = sec%3600
+#min = sec//60
+#sec = sec%60
+#print(f"{hour}:{min}:{sec}")
+
+#ЗАДАНИЕ 3
+#n = str (input ("Введите число"))
+#nn=int(n+n)
+#nnn=int(n+n+n)
+#m=int(n)+nn+nnn
+#print(m)
+
+
+
+
+
+
