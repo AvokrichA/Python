49c0a2dbb8cd18b919014d32a888a7df5a217f50
AvokrichA <leysanspb@mail.ru>
2021-11-18 17:26:25 +0300

    Lesson2

---
+++ Lesson_4/My_script.py
@@ -0,0 +1,7 @@
+from sys import argv
+if len(argv)<4
+print('Выработка в часах' time)
+print('Ставка в час', bet)
+print('Премия', prize)
+def my_salary(time, bet, prize):
+    return time*bet+prize
